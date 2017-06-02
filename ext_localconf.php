e64cec848809309da7d2e7d606eba7c38bc319e5
Sebastian Fischer <typo3@evoweb.de>
2017-06-02 17:40:36 +0200

    Cleanup ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,27 +1,28 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-
-// Register extender cache
-// needs to stay above registerAutoloader to always have settings before using the cache
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['extender'] = [
-    'frontend' => \TYPO3\CMS\Core\Cache\Frontend\PhpFrontend::class,
-    'backend' => \TYPO3\CMS\Core\Cache\Backend\FileBackend::class,
-    'groups' => [
-        'all',
-        'system',
-    ],
-    'options' => [
-        'defaultLifetime' => 0,
-    ],
-];
-
-
-if (class_exists(\Evoweb\Extender\Utility\ClassLoader::class)) {
-    \Evoweb\Extender\Utility\ClassLoader::registerAutoloader();
-}
+call_user_func(function () {
+    // Register extender cache
+    // needs to stay above registerAutoloader to always have settings before using the cache
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['extender'] = [
+        'frontend' => \TYPO3\CMS\Core\Cache\Frontend\PhpFrontend::class,
+        'backend' => \TYPO3\CMS\Core\Cache\Backend\FileBackend::class,
+        'groups' => [
+            'all',
+            'system',
+        ],
+        'options' => [
+            'defaultLifetime' => 0,
+        ],
+    ];
 
 
-// Configure clear cache post processing for extended domain model
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] =
-    'EXT:extender/Classes/Utility/ClassCacheManager.php:Evoweb\Extender\Utility\ClassCacheManager->reBuild';
+    if (class_exists(\Evoweb\Extender\Utility\ClassLoader::class)) {
+        \Evoweb\Extender\Utility\ClassLoader::registerAutoloader();
+    }
+
+
+    // Configure clear cache post processing for extended domain model
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] =
+        Evoweb\Extender\Utility\ClassCacheManager::class . '->reBuild';
+});
