fdde0f0f1293dff4f7367a7c9ee4dff8cde1730e
Sebastian Fischer <typo3@evoweb.de>
2019-08-20 22:15:51 +0200

    Change test calls

--- .Build/test.sh
+++ .Build/test.sh
@@ -31,17 +31,23 @@ runUnitTests () {
     echo "Running php lint";
     errors=$(find . -name \*.php ! -path "./.Build/*" -exec ${PHP} -d display_errors=stderr -l {} 2>&1 >/dev/null \;) && echo "$errors" && test -z "$errors"
 
-    echo "Running ${TYPO3_VERSION} unit tests in folder '${UNITTEST_FOLDER}' with suite '${UNITTEST_SUITE}'";
-    .Build/bin/phpunit --colors -c ${UNITTEST_SUITE} ${UNITTEST_FOLDER}
+    echo "Running $TYPO3_VERSION functional tests";
+    export typo3DatabaseName="typo3";
+    export typo3DatabaseHost="localhost";
+    export typo3DatabaseUsername="root";
+    export typo3DatabasePassword="";
+    export typo3DatabaseDriver="pdo_sqlite";
+    php .Build/bin/phpunit --colors -c vendor/typo3/testing-framework/Resources/Core/Build/FunctionalTests.xml Tests/Functional/;
 
     rm composer.lock
     rm -rf .Build/Web/
     rm -rf .Build/bin/
     rm -rf .Build/vendor/
     rm -rf var/
+    rm -rf vendor/
 }
 
 cd ../;
 
-runUnitTests "/usr/bin/php7.2" "^10.0.0" "~5.0.11" "" "Tests/Unit/" ".Build/Web/vendor/typo3/testing-framework/Resources/Core/Build/UnitTests.xml" "pdo_sqlite";
-runUnitTests "/usr/bin/php7.2" "dev-master as 10.0.0" "~5.0.11" "" "Tests/Unit/" ".Build/Web/vendor/typo3/testing-framework/Resources/Core/Build/UnitTests.xml" "pdo_sqlite";
+runUnitTests "/usr/bin/php7.2" "^10.0.0" "~5.0.11";
+runUnitTests "/usr/bin/php7.2" "dev-master as 10.0.0";
