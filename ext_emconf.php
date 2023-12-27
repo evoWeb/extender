7eda3d53d103665629bb9d8c7e2c6a9c092d63a2
Sebastian Fischer <typo3@evoweb.de>
2023-12-27 17:31:35 +0100

    Update ext_emconf.php
    Change depends in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF['extender'] = [
     'version' => '10.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.0-12.2.99',
+            'typo3' => '11.5.0-12.4.99',
         ],
     ],
 ];
