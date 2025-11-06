a1b9359193ed184c6b163e85dd2249818c581a91
Gabriel Kaufmann <info@typoworx.com>
2025-11-06 14:16:14 +0100

    [FIX][#42] fixing caching for missing model-proxy after cache-flush

--- Classes/Hooks/ClearCacheHook.php
+++ Classes/Hooks/ClearCacheHook.php
@@ -1,15 +1,20 @@
 <?php
-
 namespace Evoweb\Extender\Hooks;
 
+use Psr\EventDispatcher\EventDispatcherInterface;
+use TYPO3\CMS\Core\Cache\CacheManager;
+use TYPO3\CMS\Core\Cache\Event\CacheFlushEvent;
 use TYPO3\CMS\Core\Cache\Frontend\PhpFrontend;
+use TYPO3\CMS\Core\Core\BootService;
 use TYPO3\CMS\Core\Core\Environment;
 
 class ClearCacheHook
 {
-    public function __construct(protected PhpFrontend $classCache)
-    {
-    }
+    public function __construct(
+        protected PhpFrontend $classCache,
+        protected readonly BootService $bootService
+    )
+    {}
 
     /**
      * @param array<non-empty-string, string|string[]> $parameters
@@ -19,5 +24,13 @@ class ClearCacheHook
         if (Environment::getContext()->isDevelopment() && ($parameters['cacheCmd'] ?? '') === 'all') {
             $this->classCache->flush();
         }
+
+        $container = $this->bootService->getContainer(true);
+        $eventDispatcher = $container->get(EventDispatcherInterface::class);
+        $container = $this->bootService->getContainer(true);
+        $groups = $container->get(CacheManager::class)->getCacheGroups();
+
+        $event = new CacheFlushEvent($groups);
+        $eventDispatcher->dispatch($event);
     }
 }
